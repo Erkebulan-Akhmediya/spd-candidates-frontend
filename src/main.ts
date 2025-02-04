8b943807ffdde81d1bb5e792a11b262658aff76e
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-02-04 19:08:26 +0500

    removed axios plugin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'vuetify/styles'
-import axiosPlugin from './plugins/axios'
 import '@mdi/font/css/materialdesignicons.css'
 import i18n from '@/plugins/locale'
 import vuetify from '@/plugins/vuetify'
@@ -16,7 +15,6 @@ app.use(createPinia())
 app.use(router)
 app.use(i18n)
 app.use(vuetify)
-app.use(axiosPlugin)
 app.use(httpPlugin)
 app.use(filePlugin)
 
--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -1,10 +1,27 @@
 import type { App } from 'vue'
 import { HttpService } from '@/utils/HttpService.ts'
+import type { AxiosInstance } from 'axios'
+import axios from 'axios'
 
 export default {
 
   install(app: App): void {
-    app.config.globalProperties.$http = new HttpService(app.config.globalProperties.axios)
+    const axiosInstance: AxiosInstance = axios.create({
+      baseURL: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}`,
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+    });
+
+    axiosInstance.interceptors.request.use(
+      (config) => {
+        const token: string | null = sessionStorage.getItem('token')
+        if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+      },
+      (error) => Promise.reject(error),
+    );
+
+    app.config.globalProperties.$http = new HttpService(axiosInstance)
   }
 
 }
