3f9e4a226d56d98a1848337ecd3eb92e75a87e02
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-02-11 21:49:39 +0500

    fixed point distribution creation logic

--- src/components/TestForm/PointDistributionOptionConstructor.vue
+++ src/components/TestForm/PointDistributionOptionConstructor.vue
@@ -1,6 +1,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import OptionScaleSelector from '@/components/TestForm/OptionScaleSelector.vue'
+import { mapWritableState } from 'pinia'
+import { useTestStore } from '@/stores/test.ts'
+import type { OptionToCreate } from '@/interfaces/option.ts'
 
 export default defineComponent({
   name: 'PointDistributionOptionConstructor',
@@ -20,14 +23,53 @@ export default defineComponent({
       required: true
     }
   },
+
+  data() {
+    return {
+      loading: false,
+    }
+  },
+
+  computed: {
+    ...mapWritableState(useTestStore, ['test']),
+    optionName(): string {
+      return String.fromCharCode('a'.charCodeAt(0) + this.optionIndex)
+    },
+    currentOption(): OptionToCreate {
+      return this.test
+        .variants[this.variantIndex]
+        .questions[this.questionIndex]
+        .options[this.optionIndex]
+    }
+  },
+
+  mounted() {
+    this.loading = true
+    this.setOption()
+    this.loading = false
+  },
+
+  methods: {
+    setOption() {
+      this.test.variants[this.variantIndex].questions[this.questionIndex].options[this.optionIndex] = {
+        withFile: false,
+        file: null,
+        nameRus: this.optionName,
+        nameKaz: this.optionName,
+        isCorrect: null
+      }
+    }
+  },
+
 })
 </script>
 
 <template>
   <v-list-item-title>
-    <v-row justify="space-between" align="center" class="pa-3">
+    <v-progress-circular v-if="loading" />
+    <v-row v-else justify="space-between" align="center" class="pa-3">
       <v-col cols="3">
-        <h3>Ответ</h3>
+        <h3>Ответ {{currentOption.nameRus}}</h3>
       </v-col>
       <v-col cols="3">
         <option-scale-selector />
--- src/services/TestConverterService.ts
+++ src/services/TestConverterService.ts
@@ -1,18 +1,13 @@
 import type { OptionToCreate, OptionToSend } from '@/interfaces/option.ts'
 import type { QuestionToCreate, QuestionToSend } from '@/interfaces/question.ts'
 import type { VariantToCreate, VariantToSend } from '@/interfaces/variant.ts'
-import { type TestToCreate, type TestToSend, TestType } from '@/interfaces/test.ts'
+import { type TestToCreate, type TestToSend } from '@/interfaces/test.ts'
 import type FileService from '@/services/FileService.ts'
 
 export default class TestConverterService {
   private static instance: TestConverterService
-  private testType: TestType
-  private maxPointsPerQuestion: number
 
   private constructor(private readonly file: FileService) {
-    // дефолтные значения, выбраны случайно
-    this.testType = 1
-    this.maxPointsPerQuestion = 1
   }
 
   public static getInstance(file: FileService): TestConverterService {
@@ -23,8 +18,6 @@ export default class TestConverterService {
   }
 
   public async convertTestToSend(test: TestToCreate): Promise<TestToSend> {
-    this.testType = test.type
-    this.maxPointsPerQuestion = test.maxPointsPerQuestion
     return {
       ...test,
       variants: await Promise.all(test.variants.map(this.convertVariantToSend.bind(this))),
@@ -39,45 +32,11 @@ export default class TestConverterService {
   }
 
   private async convertQuestionToSend(question: QuestionToCreate): Promise<QuestionToSend> {
-    let options: OptionToSend[]
-    if (this.testType === TestType.pointDistribution) {
-      options = this.createOptionsForPointDistribution()
-    } else {
-      options = await Promise.all(question.options.map(this.convertOptionToSend.bind(this)))
-    }
-
     return {
       ...question,
       fileName: question.withFile ? await this.file.upload(question.file!) : null,
-      options: options,
+      options: await Promise.all(question.options.map(this.convertOptionToSend.bind(this))),
     }
-  }
-
-  private createOptionsForPointDistribution(): OptionToSend[] {
-    let name: string = 'a'
-    const optionsToSend: OptionToSend[] = []
-    for (let i: number = 0; i < this.maxPointsPerQuestion; i++) {
-      optionsToSend.push({
-        withFile: false,
-        fileName: null,
-        nameRus: name,
-        nameKaz: name,
-        isCorrect: null,
-      })
-      name = this.getNextChar(name)
-    }
-    return optionsToSend
-  }
-
-  private getNextChar(char: string): string {
-    if (char.length !== 1) throw 'character is required'
-
-    char = char.toLowerCase()
-    if (char === 'z') throw 'Достигнуто максимальное количество варинатов ответа'
-
-    const code: number = char.charCodeAt(0)
-
-    return String.fromCharCode(code + 1).toLowerCase()
   }
 
   private async convertOptionToSend(option: OptionToCreate): Promise<OptionToSend> {
